a5495f86ce2dfd4c57ffbe91aec8d3f714b2d438
tproulund <tproulund@gmail.com>
2017-04-16 11:00:38 +0200

    update

--- scripts/matlab/kinematics.m
+++ scripts/matlab/kinematics.m
@@ -3,17 +3,26 @@
 clear all
 clc
 close all
-addpath('plot_functions/')
-addpath('V-REP_PRO_EDU_V3_3_2_64_Linux/programming/remoteApiBindings/matlab/matlab/')
-addpath('V-REP_PRO_EDU_V3_3_2_64_Linux/programming/remoteApiBindings/lib/lib/64Bit/')
+addpath('./functions/')
+addpath('trs/matlab/')
+startup_robot
+
 
 %% Geometri
 d1 = 0.387; %m
 a1 = 0.325; %m
 a2 = 0.6-a1; %m
+a4 = 0.1;
 d4 = -0.05; %m
+alpha4 = 0;
 
-constants = [a1 a2 d1 d4];
+
+%% Camera
+alpha_c = 0; % eventual z rotation of camera
+p_c = [0 0 0]; %vector from camera to base
+
+%% Constants vector
+constants = [a1 a2 d1 d4 a4 alpha4 alpha_c p_c];
 %% Joint ranges
 q1_range = [-105 105]*pi/180;
 q2_range = [-150 150]*pi/180;
@@ -22,7 +31,7 @@ q4_range = [-pi pi]*2;
 
 %% Kinematic and invers kinematics plot
 
-q = [-1 -1 -0.15 2]'; %joint space configuration
+q = [0 -pi/2 -0.1 -2]'; %joint space configuration
 p = [0.2 -0.2 0.237 0.6]'; % end effector position and orientation
 
 %figure
@@ -32,7 +41,7 @@ p = [0.2 -0.2 0.237 0.6]'; % end effector position and orientation
 % If p is added as the last argument, the function calculates the inverse
 % kinematics 
 
-[q_new p_new] = plot_of_scara(q, constants, @kinematric_transform, @plot_functions,p)
+[q_new p_new] = plot_of_scara(q, constants, @kinematric_transform, @plot_functions)
 
 %drawnow
  
